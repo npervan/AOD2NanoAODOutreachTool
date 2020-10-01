0fac457ddbe129cc9d1ff2ecc62ef4ba12c22127
Nikolas Pervan <nikolas_pervan@brown.edu>
2020-10-01 05:07:18 +0200

    updated data config as well--both seg fault after closing output file

--- configs/data_cfg.py
+++ configs/data_cfg.py
@@ -2,6 +2,11 @@ import FWCore.ParameterSet.Config as cms
 import FWCore.Utilities.FileUtils as FileUtils
 import FWCore.PythonUtilities.LumiList as LumiList
 import FWCore.ParameterSet.Types as CfgTypes
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+import os 
+
+relBase = os.environ['CMSSW_BASE']
+
 process = cms.Process("AOD2NanoAOD")
 process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
@@ -36,8 +41,9 @@ process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 
 # Set global tag
 # We don't have set the global tag for the educational samples. This simplifies running the code since we don't have to access the database.
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = "FT_R_53_V18::All"
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "FT_R_53_V18::All"
 
 # Apply JSON file with lumi mask (needs to be done after the process.source definition)
 goodJSON = "data/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt"
@@ -45,6 +51,52 @@ myLumis = LumiList.LumiList(filename=goodJSON).getCMSSWString().split(",")
 process.source.lumisToProcess = CfgTypes.untracked(
     CfgTypes.VLuminosityBlockRange())
 process.source.lumisToProcess.extend(myLumis)
+
+# Load PAT config                                                                                                                     
+process.load("RecoTauTag.Configuration.RecoPFTauTag_cff") # re-run tau discriminators (new version)                               
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+process.load('RecoJets.Configuration.RecoJets_cff')
+process.load('RecoJets.JetProducers.TrackJetParameters_cfi')
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+
+# Configure PAT to use PF2PAT instead of AOD sources                                                                                 
+# this function will modify the PAT sequences.                                                                                              
+from PhysicsTools.PatAlgos.tools.pfTools import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+from PhysicsTools.PatAlgos.tools.metTools import *
+from PhysicsTools.PatAlgos.tools.jetTools import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+
+process.goodOfflinePrimaryVertices = cms.EDFilter(
+    "VertexSelector",
+    filter = cms.bool(False),
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.rho < 2")
+    )
+
+process.ak5PFJets.doAreaFastjet = True
+addPfMET(process, 'PF')
+
+addJetCollection(process,cms.InputTag('ak5PFJets'),
+                 'AK5', 'PFCorr',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5PF', cms.vstring(['L1FastJet','L2Relative','L3Absolute'])),#'L2L3Residual'])),          
+                 doType1MET   = True,
+                 doL1Cleaning = True,
+                 doL1Counters = False,
+                 doJetID      = True,
+                 jetIdLabel   = "ak5",
+                 #outputModules= ['out']                                                                                   
+                 )
+
+from RecoMET.METFilters.trackingFailureFilter_cfi import trackingFailureFilter
+process.trackingFailureFilter = trackingFailureFilter.clone()
+process.trackingFailureFilter.VertexSource = cms.InputTag('goodOfflinePrimaryVertices')
+
 
 # Number of events to be skipped (0 by default)
 process.source.skipEvents = cms.untracked.uint32(0)
@@ -62,4 +114,4 @@ process.aod2nanoaod = cms.EDAnalyzer("AOD2NanoAOD",
 process.TFileService = cms.Service(
     "TFileService", fileName=cms.string("output.root"))
 
-process.p = cms.Path(process.aod2nanoaod)
+process.p = cms.Path(process.patDefaultSequence * process.aod2nanoaod)
